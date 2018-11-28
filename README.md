02744419e95b4144c8942aa77bd867b5d36ed23f
pskanade <k.pranav.apk@gmail.com>
2018-11-29 00:09:44 +0530

    The first commit to master

---
+++ README.md
@@ -0,0 +1,8 @@
+# ferrets.io - dev
+
+Code for the website. Trying to keep the two things seprate.
+
+## Following is the plan:
+
+1. Create a cronjob to push the changes every now and then.
+2. Build a basic front end that suits.
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <p>
+      Thanks for visiting..! We are in process of building this website. Please
+      check up on us after few days. :)
+    </p>
+  );
+};
