e84ee2ebb2f30f2e2a3c616bc4879d334e7fa538
pskanade <k.pranav.apk@gmail.com>
2018-12-03 23:42:36 +0530

    Fixed the alignment issue with the nav bar menu items. Removed the menu activation and made it fixed.

--- src/Components/Layouts/Navbar.js
+++ src/Components/Layouts/Navbar.js
@@ -1,98 +1,62 @@
-import React, { Component } from "react";
-import { Menu } from "semantic-ui-react";
+import React, {Component} from "react";
+import {Menu} from "semantic-ui-react";
 import classes from "../../../static/css/Navbar.css";
-import { Link as ScrollLink } from "react-scroll";
+import {Link as ScrollLink} from "react-scroll";
 
-export default class Navbar extends Component {
-  state = { activeItem: "home" };
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
+export default class  extends Component {
+  state = {
+    activeItem: "home"
+  };
+  
+  menuItems = [
+    {
+      itemName: "what",
+      itemTag: "What"
+    },
+    {
+      itemName: "why",
+      itemTag: "Why"
+    },
+    {
+      itemName: "when",
+      itemTag: "When"
+    },
+    {
+      itemName: "who",
+      itemTag: "Who"
+    }
+  ];
+  
+  renderMenuItem = (itemName, itemTag, activeItem) => {
+    return (
+      <Menu.Item
+        name={itemName}
+        active={activeItem === itemName}
+      >
+        <ScrollLink
+          to={itemName}
+          spy={true}
+          smooth={true}
+          duration={700}
+        >
+          <b>{itemTag}</b>
+        </ScrollLink>
+      </Menu.Item>
+    );
+  };
+  
   render() {
-    const { activeItem } = this.state;
-
+    const activeItem = "home";
     return (
       <div className={classes.navbar}>
         <Menu borderless fixed="top">
-          <Menu.Item
-            name="home"
-            active={activeItem === "home"}
-            onClick={this.handleItemClick}
-          >
-            <ScrollLink
-              to="home"
-              spy={true}
-              smooth={true}
-              duration={700}
-              id="sclink"
-            >
-              <b>ferrets.io</b>
-            </ScrollLink>
-          </Menu.Item>
+          {this.renderMenuItem("home", "ferrets.io", activeItem)}
           <Menu.Menu position="right">
-            <Menu.Item
-              name="what"
-              active={activeItem === "what"}
-              onClick={this.handleItemClick}
-            >
-              <ScrollLink
-                to="what"
-                spy={true}
-                smooth={true}
-                duration={700}
-                id="sclink"
-              >
-                <b>What</b>
-              </ScrollLink>
-            </Menu.Item>
-            <Menu.Item
-              name="why"
-              active={activeItem === "why"}
-              onClick={this.handleItemClick}
-            >
-              <ScrollLink
-                to="why"
-                spy={true}
-                smooth={true}
-                duration={700}
-                id="sclink"
-              >
-                <b>Why</b>
-              </ScrollLink>
-            </Menu.Item>
-            <Menu.Item
-              name="When"
-              active={activeItem === "When"}
-              onClick={this.handleItemClick}
-            >
-              <ScrollLink
-                to="when"
-                spy={true}
-                smooth={true}
-                duration={700}
-                id="sclink"
-              >
-                <b>When</b>
-              </ScrollLink>
-            </Menu.Item>
-            <Menu.Item
-              name="Who"
-              active={activeItem === "Who"}
-              onClick={this.handleItemClick}
-            >
-              <ScrollLink
-                to="who"
-                spy={true}
-                smooth={true}
-                duration={700}
-                id="sclink"
-              >
-                <b>Who</b>
-              </ScrollLink>
-            </Menu.Item>
+            {this.menuItems.map(mItem => this.renderMenuItem(mItem.itemName, mItem.itemTag, activeItem))}
           </Menu.Menu>
         </Menu>
       </div>
     );
   }
 }
+
